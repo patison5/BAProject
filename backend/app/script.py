503913c16c898f1a2e2f2833a54695e87995f680
EugeneOne1 <gubaldulnzulfia@gmail.com>
2020-07-22 22:34:07 +0300

    allowed extensions changed

--- backend/app/script.py
+++ backend/app/script.py
@@ -16,7 +16,7 @@ env = Environment(
 )
 
 UPLOAD_FOLDER = './static/uploads/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'svg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
